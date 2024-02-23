c271c71a16c272867c92314037dc64e9b9d35ae3
jsd1982 <james.jdunne@gmail.com>
2024-02-22 22:43:35 -0600

    Read query batches from stdin until GO separator; improve column value formatting for various types

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"database/sql"
 	"encoding/csv"
 	"encoding/hex"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/google/uuid"
 	_ "github.com/microsoft/go-mssqldb"
-	"io"
+	mssql "github.com/microsoft/go-mssqldb"
 	"os"
 	"strconv"
 	"strings"
@@ -17,80 +19,118 @@ import (
 )
 
 func main() {
+	csEnv := flag.String("csenv", "", "get sql connection string from this environment variable")
 	csTmpl := flag.String("cs", "", "sql connection string")
 	nullStrValue := flag.String("null", "NULL", "null string representation to use in CSV output")
 	queryTimeoutSec := flag.Int("t", 60, "query timeout (seconds)")
 
 	flag.Parse()
 
-	var driverName string
-	var connectionString string
-
-	driverName = "sqlserver"
-	connectionString = *csTmpl
+	connectionString := *csTmpl
 	if connectionString == "" {
-		fmt.Fprintln(os.Stderr, "missing required sql connection string flag -cs")
-		os.Exit(1)
+		// fetch name of environment variable:
+		envName := *csEnv
+		if envName == "" {
+			_, _ = fmt.Fprintln(os.Stderr, "missing required sql connection string via -cs or -csenv flag")
+			os.Exit(1)
+		}
+
+		connectionString = os.Getenv(envName)
+		if connectionString == "" {
+			_, _ = fmt.Fprintf(os.Stderr, "missing required sql connection string from environment variable '%s' (via -csenv flag)\n", envName)
+			os.Exit(1)
+		}
 	}
 
 	var err error
 	var c *sql.DB
 	if c, err = sql.Open(
-		driverName,
+		"sqlserver",
 		connectionString,
 	); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	defer c.Close()
+	defer func(c *sql.DB) {
+		_ = c.Close()
+	}(c)
 
 	{
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 
 		// test database connectivity with a quick Ping():
 		if err = c.PingContext(ctx); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			_, _ = fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 
 		cancel()
 	}
 
-	// read all query text from stdin until EOF:
-	var text []byte
-	text, err = io.ReadAll(os.Stdin)
+	var text strings.Builder
 
-	// start a CSV writer:
-	cw := csv.NewWriter(os.Stdout)
+	// read all query text from stdin:
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-	// execute the query and write CSV output:
-	err = (&QueryCSV{
-		c:            c,
-		cw:           cw,
-		nullString:   *nullStrValue,
-		queryTimeout: time.Second * time.Duration(*queryTimeoutSec),
-	}).execQuery(string(text))
+		// ready to execute?
+		if strings.ToUpper(strings.TrimSpace(line)) == "GO" {
+			_, _ = os.Stdout.WriteString("\n")
 
-	// make sure CSV flushes to stdout:
-	cw.Flush()
+			// start a CSV writer:
+			cw := csv.NewWriter(os.Stdout)
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+			// execute the query and write CSV output:
+			q := &queryCSV{
+				c:            c,
+				cw:           cw,
+				nullString:   *nullStrValue,
+				queryTimeout: time.Second * time.Duration(*queryTimeoutSec),
+			}
+			err = q.execQuery(text.String())
+
+			// make sure CSV flushes to stdout:
+			cw.Flush()
+
+			// handle any errors:
+			if err != nil {
+				var sqlErr mssql.Error
+				if errors.As(err, &sqlErr) {
+					// SQL server error:
+					_, _ = fmt.Fprintf(os.Stderr, "%#v\n", sqlErr)
+				} else {
+					_, _ = fmt.Fprintln(os.Stderr, err)
+				}
+			} else {
+				// report row count:
+				_, _ = fmt.Fprintf(os.Stderr, "%d row(s) queried\n", q.rowCount)
+			}
+
+			// prepare for next query:
+			text.Reset()
+		} else {
+			// nope; append line to text:
+			text.WriteString(line)
+			text.WriteString("\r\n")
+		}
 	}
 
-	os.Stdout.Sync()
-	os.Stderr.Sync()
+	if err = scanner.Err(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
-type QueryCSV struct {
+type queryCSV struct {
 	c            *sql.DB
 	cw           *csv.Writer
 	nullString   string
 	queryTimeout time.Duration
+	rowCount     int
 }
 
-func (q *QueryCSV) execQuery(text string) (err error) {
+func (q *queryCSV) execQuery(text string) (err error) {
 	var rows *sql.Rows
 
 	ctx, cancel := context.WithTimeout(context.Background(), q.queryTimeout)
@@ -106,63 +146,31 @@ func (q *QueryCSV) execQuery(text string) (err error) {
 	_, _ = tStart, tEnd
 
 	if err != nil {
-		return
+		return fmt.Errorf("error executing query: %w", err)
 	}
 
 	var colTypes []*sql.ColumnType
 	if colTypes, err = rows.ColumnTypes(); err != nil {
-		return
+		return fmt.Errorf("error fetching column schema: %w", err)
+	}
+
+	// write the CSV header:
+	colNames := q.writeHeader(colTypes)
+	if err = q.cw.Write(colNames); err != nil {
+		return fmt.Errorf("error writing CSV column header: %w", err)
 	}
 
 	cw := q.cw
 
-	colNames := make([]string, len(colTypes))
-	for i := range colTypes {
-		nullable, hasNullable := colTypes[i].Nullable()
-		length, hasLength := colTypes[i].Length()
-		decimalSize, decimalScale, hasDecimalSize := colTypes[i].DecimalSize()
-
-		sb := strings.Builder{}
-		colName := colTypes[i].Name()
-		/*if colName != ""*/ {
-			sb.WriteRune('[')
-			sb.WriteString(strings.ReplaceAll(colName, "]", "]]"))
-			sb.WriteRune(']')
-			sb.WriteRune(' ')
-		}
-		sb.WriteString(colTypes[i].DatabaseTypeName())
-		if hasLength {
-			sb.WriteRune('(')
-			sb.WriteString(strconv.FormatInt(length, 10))
-			sb.WriteRune(')')
-		} else if hasDecimalSize {
-			sb.WriteRune('(')
-			sb.WriteString(strconv.FormatInt(decimalSize, 10))
-			sb.WriteRune(',')
-			sb.WriteString(strconv.FormatInt(decimalScale, 10))
-			sb.WriteRune(')')
-		}
-		if hasNullable {
-			sb.WriteRune(' ')
-			if !nullable {
-				sb.WriteString("NOT ")
-			}
-			sb.WriteString("NULL")
-		}
-		colNames[i] = sb.String()
-	}
-
-	cw.Write(colNames)
-
-	formatted := make([]string, len(colNames))
-	rowValues := make([]any, len(colNames))
-	for rowCounter := 0; rows.Next(); rowCounter++ {
+	formatted := make([]string, len(colTypes))
+	rowValues := make([]any, len(colTypes))
+	for q.rowCount = 0; rows.Next(); q.rowCount++ {
 		// fetch column values:
 		for i := range rowValues {
 			rowValues[i] = new(any)
 		}
 		if err = rows.Scan(rowValues...); err != nil {
-			return
+			return fmt.Errorf("error in row %d scanning: %w", q.rowCount+1, err)
 		}
 
 		// format column values for output to CSV:
@@ -179,9 +187,18 @@ func (q *QueryCSV) execQuery(text string) (err error) {
 				var uv uuid.UUID
 				uv, err = uuid.FromBytes(value.([]byte))
 				if err != nil {
-					return
+					return fmt.Errorf("error in row %d constructing uuid from bytes: %w", q.rowCount+1, err)
 				}
 				formatted[i] = uv.String()
+			case "DECIMAL":
+			case "MONEY":
+				formatted[i] = string(value.([]byte))
+			case "BIT":
+				if value.(bool) {
+					formatted[i] = "1"
+				} else {
+					formatted[i] = "0"
+				}
 			default:
 				switch v := value.(type) {
 				case []byte:
@@ -194,23 +211,66 @@ func (q *QueryCSV) execQuery(text string) (err error) {
 
 		// write the CSV line:
 		if err = cw.Write(formatted); err != nil {
-			return
+			return fmt.Errorf("error in row %d writing CSV: %w", q.rowCount+1, err)
 		}
 
-		// make sure CSV flushes to stdout:
-		cw.Flush()
-
 		if err = cw.Error(); err != nil {
-			return
+			return fmt.Errorf("error in row %d writing CSV: %w", q.rowCount+1, err)
 		}
 	}
 
 	if err = rows.Close(); err != nil {
-		return
+		return fmt.Errorf("error closing result set: %w", err)
 	}
 
 	if err = rows.Err(); err != nil {
-		return
+		return fmt.Errorf("error from result set: %w", err)
+	}
+
+	return
+}
+
+func (q *queryCSV) writeHeader(colTypes []*sql.ColumnType) (colNames []string) {
+	colNames = make([]string, len(colTypes))
+
+	// output column header including types:
+	for i := range colTypes {
+		nullable, hasNullable := colTypes[i].Nullable()
+		length, hasLength := colTypes[i].Length()
+		decimalSize, decimalScale, hasDecimalSize := colTypes[i].DecimalSize()
+
+		sb := strings.Builder{}
+		colName := colTypes[i].Name()
+		/*if colName != ""*/ {
+			sb.WriteRune('[')
+			sb.WriteString(strings.ReplaceAll(colName, "]", "]]"))
+			sb.WriteRune(']')
+			sb.WriteRune(' ')
+		}
+		sb.WriteString(colTypes[i].DatabaseTypeName())
+		if hasLength {
+			sb.WriteRune('(')
+			if length == 2147483645 || length == 1073741822 {
+				sb.WriteString("max")
+			} else {
+				sb.WriteString(strconv.FormatInt(length, 10))
+			}
+			sb.WriteRune(')')
+		} else if hasDecimalSize {
+			sb.WriteRune('(')
+			sb.WriteString(strconv.FormatInt(decimalSize, 10))
+			sb.WriteRune(',')
+			sb.WriteString(strconv.FormatInt(decimalScale, 10))
+			sb.WriteRune(')')
+		}
+		if hasNullable {
+			sb.WriteRune(' ')
+			if !nullable {
+				sb.WriteString("NOT ")
+			}
+			sb.WriteString("NULL")
+		}
+		colNames[i] = sb.String()
 	}
 
 	return
